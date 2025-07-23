2cf23483c464e3c0334b2bc3b5b196530cc483c4
ody811 <123749723+ody811@users.noreply.github.com>
2025-07-24 00:03:58 +0200

    index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
 const { createGame, joinGame, playMove } = require("./gameLogic");
-
+ 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
